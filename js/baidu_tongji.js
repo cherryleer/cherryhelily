e8fe774346451223a79fb33610bbfb94f0189cf6
cherryleer <cherryleer@gmail.com>
2015-07-12 13:30:04 +0800

    add baidu_tongji.js

---
+++ js/baidu_tongji.js
@@ -0,0 +1,10 @@
+/**
+ * Created by cherryleer on 7/12/15.
+ */
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?5421b82a1548a79ac4428fe860057a31";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
